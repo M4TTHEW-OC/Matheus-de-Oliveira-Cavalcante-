968addfafebd2f8eb24d13ebc0171130d0fcb1a3
M4TTHEW-OC <99699238+M4TTHEW-OC@users.noreply.github.com>
2022-02-16 23:52:01 -0300

    Update Média.cpp

--- Média.cpp
+++ Média.cpp
@@ -1,8 +1,10 @@
-/* Leia os doi valores de dois pontos flutuantes de presição de A e B correspondentes ás notas de dois alunos .Após isso, calcule a média do aluno,considerando
-que a nota A tem peso 3,5 e B tem peso 7,5 .Cada noto pode ser de zero a dez , sempre com um dígito depois após da virgula .Não se esqueça de imprimir o final da linha
-apóso resultado, caso contrário você receberá "Erro de apresentação ".Não se esqueça do espaço antes e depois do sinal de igual.*/
+/* Leia os doi valores de dois pontos flutuantes de presiÃ§Ã£o de A e B correspondentes Ã¡s notas de dois alunos .ApÃ³s isso, calcule a mÃ©dia do aluno,considerando
+que a nota A tem peso 3,5 e B tem peso 7,5 .Cada noto pode ser de zero a dez , sempre com um dÃ­gito depois apÃ³s da virgula .NÃ£o se esqueÃ§a de imprimir o final da linha
+apÃ³so resultado, caso contrÃ¡rio vocÃª receberÃ¡ "Erro de apresentaÃ§Ã£o ".NÃ£o se esqueÃ§a do espaÃ§o antes e depois do sinal de igual. ?*/
 
 #include <stdio.h>
+#define P1 3.5 
+#define P2 7.5
 int main(){
 	float n1a, n2a, amedia, n1b, n2b, bmedia;
 	
@@ -12,7 +14,7 @@ int main(){
 	printf("Digite a Nota B do primeiro Aluno: ");
 	scanf("%f", &n2a);
 	
-	amedia = (n1a * 3.5 + n2a * 7.5) /2 ;
+	amedia = (n1a * P1 + n2a * P2) /11 ;
 	 
 	 printf("Digite a Nota A do segundo Aluno: ");
 	scanf("%f", &n1a);
@@ -20,8 +22,8 @@ int main(){
 	printf("Digite a Nota B do segundo Aluno: ");
 	scanf("%f", &n2a);
 	
-	bmedia = (n1a * 3.5 + n2a * 7.5) /2 ;
-	 printf("A media do aluno A e: %f\n", &amedia);
+	bmedia = (n1a * P1 + n2a * P2) /11 ;
+	 printf("A media do aluno A e: %f\n", amedia);
 	 
-	 printf("A media do aluno B e: %f", &bmedia);
+	 printf("A media do aluno B e: %f", bmedia);
 }
