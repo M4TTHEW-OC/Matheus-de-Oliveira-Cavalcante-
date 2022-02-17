394c35236644f2c99ec15d7b2bdd5e312269427d
M4TTHEW-OC <99699238+M4TTHEW-OC@users.noreply.github.com>
2022-02-17 00:00:38 -0300

    Update área do circulo.cpp

--- área do circulo.cpp
+++ área do circulo.cpp
@@ -11,8 +11,7 @@ double r, resultado;
 
 printf("Digite o valor do Raio :");
 scanf("%lf", &r);
-
-resultado = r * r * 3.14159; 
+ 
 resultado = pow(raio,2) * PI;
 printf("O resultado da area do circulo e : %.1lf ", resultado); // quando adicionar .x antes do lf indica a quantida de casas depois da virgula .
 
