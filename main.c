397b87c49473b371ff4efc4ce088aa3b31ad3afb
MATHEUS DE OLIVEIRA CAVALCANTE <99699238+M4TTHEW-OC@users.noreply.github.com>
2022-04-27 11:35:11 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,47 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-
- 
 int main() {
- 
-	float notas[10], menor, maior;
-	int i = 0,  tam = 0;
-	char continuar = 's';
- 
-	do{
-		printf("Informe a nota %d:",i+1);
-		scanf("%f",&notas[i]);	
-		i++;
- 
-		printf("Deseja continuar (s/n)?");
-		scanf(" %c",&continuar);	
-	}while(i<10 && continuar=='s');
- 
- 
-	printf("--------------\n");
- 
-	tam = i;
- 
-	for(i=0;i<tam;i++){
- 
-		if(i==0){
-			menor = notas[i];
-			maior = notas[i];
-		}else{
-			if(notas[i]<menor){
-				menor=notas[i];
-			}			
-			if(notas[i]>maior){
-				maior=notas[i];
-			}
-		}
- 
-	}
- 
-	printf("--------------\n");
-	printf("A menor nota informada foi: %.1f\n",menor);
-	printf("A maior nota informada foi: %.1f\n",maior);
- 
+	int opcao, i;
+for(i=0; i <4; i++){
+printf("Selecione uma das opcoes a seguir: \n1. Soma n2. multiplacacao n3. divisao  n4. subtracao :");
+scanf("%d", &opcao);
+
+	switch(opcao){
+		case '1':
+			double soma1, soma2;
+			printf("Selecione seu primeiro numero: ");
+			scanf("%lf", &soma1);
+			printf("Selecione seu segundo numero: ");
+			scanf("%lf", &soma2);
+			printf("Seu resultado foi: %.2lf\n", soma1 + soma2);
+			break;
+		case '2':
+			double mult1, mult2;
+			printf("Selecione seu primeiro numero: ");
+			scanf("%lf", &mult1);
+			printf("Selecione seu segundo numero: ");
+			scanf("%lf", &mult2);
+			printf("Seu resultado foi: %.2lf\n", mult1 * mult2);
+			break;
+		case '3':
+			double div1, div2;
+			printf("Digite o seu primeiro numero: ");
+			scanf("%lf", &div1);
+			printf("Digite o seu segundo numero: ");
+			scanf("%lf", &div2);
+			printf("Sua divisao é %lf\n", div1 / div2 );
+			break;
+		case '4':
+			double sub1, sub2;
+			printf("Selecione seu primeiro numero: ");
+			scanf("%lf", &sub1);
+			printf("Selecione seu segundo numero: ");
+			scanf("%lf", &sub2);
+			printf("Seu resultado foi: %.2lf\n", sub1 - sub2);
+			break;
+		default: printf("Numero Invalido\n");
+	}	
+}
+	
+	
 	return 0;
 }
