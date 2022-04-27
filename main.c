0770c14798e8fd4c68dedcad397d7cb133a29b73
MATHEUS DE OLIVEIRA CAVALCANTE <99699238+M4TTHEW-OC@users.noreply.github.com>
2022-04-27 11:35:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ scanf("%d", &opcao);
 			scanf("%lf", &div1);
 			printf("Digite o seu segundo numero: ");
 			scanf("%lf", &div2);
-			printf("Sua divisao � %lf\n", div1 / div2 );
+			printf("Sua divisao é %lf\n", div1 / div2 );
 			break;
 		case '4':
 			double sub1, sub2;
@@ -48,3 +48,156 @@ scanf("%d", &opcao);
 	
 	return 0;
 }
+
+/*
+#include <stdio.h>
+ 
+ 
+void head(); // 2 - protótipo do procedimento
+float soma(float n1, float num2); // 2 - protótipo da função
+char le_valida_operador();
+ 
+int main() {
+ 
+	float n1,n2,result;
+	char op;
+ 
+	head(); // 3 - chamada do procedimento
+ 
+	printf("N1: ");
+	scanf("%f",&n1);
+ 
+	printf("OP:");
+	op = le_valida_operador();
+ 
+	printf("N2: ");
+	scanf("%f",&n2);
+ 
+	printf("---------------\n");
+ 
+	switch(op){
+		case '+':
+			result = soma(n1,n2);
+			printf("RESULTADO:   %.1f\n",result); // 3 - chamada da função soma()
+		break;
+		case '-':
+			printf("RESULTADO:   %.1f\n",n1-n2);
+		break;
+		case '*':
+			printf("RESULTADO:   %.1f\n",n1*n2);
+		break;
+		case '/':
+			printf("RESULTADO:   %.1f\n",n1/n2);
+		break;						
+ 
+		default:
+			printf("OPERADOR INVALIDO");
+ 
+	}
+ 
+	return 0;
+}
+ 
+// 1 - declaração do procedimento
+void head(){
+	printf("---------------\n");
+	printf("CALC\n");
+	printf("---------------\n");	
+}
+ 
+// 1 - declaração da função
+float soma(float n1, float num2){
+	float resultado;
+	resultado = n1 + num2;
+	return resultado;
+}
+ 
+ 
+char le_valida_operador(){
+	char operador;
+	int cont=0;
+	do{
+		if(cont!=0){
+			printf("-------\nERRO\n-------\n");
+		}
+		scanf(" %c",&operador);	
+		cont++;
+	}while(operador!='+' && operador!='-' && operador!='*' && operador!='/');	
+ 
+	return operador;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// https://pastebin.com/akbWN6si
+/*
+#include <stdio.h>
+ 
+int main() {
+ 
+	float n1,n2;
+	char op;
+ 
+	printf("---------------\n");
+	printf("CALC\n");
+	printf("---------------\n");
+ 
+	printf("N1: ");
+	scanf("%f",&n1);
+ 
+	printf("OP:");
+	scanf(" %c",&op); 
+ 
+	printf("N2: ");
+	scanf("%f",&n2);
+ 
+ 
+	printf("---------------\n");
+ 
+ 
+ 
+	switch(op){
+		case '+':
+			printf("RESULTADO:   %.1f\n",n1+n2);
+		break;
+		case '-':
+			printf("RESULTADO:   %.1f\n",n1-n2);
+		break;
+		case '*':
+			printf("RESULTADO:   %.1f\n",n1*n2);
+		break;
+		case '/':
+			printf("RESULTADO:   %.1f\n",n1/n2);
+		break;						
+ 
+		default:
+			printf("OPERADOR INVALIDO");
+ 
+	}
+ 
+	return 0;
+}
+*/
