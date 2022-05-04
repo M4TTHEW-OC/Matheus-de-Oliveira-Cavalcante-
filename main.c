b7135cf8a9cf865da0cfec957be059a655194c63
MATHEUS DE OLIVEIRA CAVALCANTE <99699238+M4TTHEW-OC@users.noreply.github.com>
2022-05-04 11:28:49 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,51 +1,115 @@
 #include <stdio.h>
- 
-// 2 - prototipação
-int em_uma_decada(int minha_idade);
+#include <string.h>
+#include <ctype.h>
  
 int main() {
-	int idade = 0, idade_mais_10 = 0;
-	printf("Informe a idade: ");
-	scanf("%d",&idade);
+    char nome[50], letra;
+    int tamanho = 0, i = 0, contador = 0;
+    
+    strcpy(nome,"Diego Rodrigues"); //#1
+    printf("Nome: %s\n", nome);
+    
+    printf("Tamanho: %d\n", strlen(nome)); //#2
+    
+    tamanho = strlen(nome);
+    
+    printf("Primeira letra: %c\n",nome[0]);
+    printf("Ultima letra: %c\n",nome[tamanho-1]);
+    
+    //3 ctype.h
+    printf("Primeira letra upper: %c\n", toupper(nome[0]));
+    printf("Primeira letra lower: %c\n", tolower(nome[0]));
  
-// 3 - chamada da função
-	idade_mais_10 = em_uma_decada(idade);
-	printf("%d\n",em_uma_decada(idade));
-	printf("%d\n",idade_mais_10);
-	printf("Valores acima em uma decada com base na idade informada: %d\n",idade);
+    printf("Ultima letra upper: %c\n", toupper(nome[tamanho-1]));
+    printf("Ultima letra lower: %c\n", tolower(nome[tamanho-1]));
+    
+    printf("Nome originalmente informado: %s\n", nome);
+    
+    printf("Nome na vertical\n");
+    for(i=0;i<tamanho;i++){
+        printf("%c\n", nome[i]);    
+    }
+    
+    printf("Nome Upper\n");
+    for(i=0;i<tamanho;i++){
+        printf("%c", toupper(nome[i])); 
+    }
+    
+    printf("\nNome invertido\n");
+    for(i=tamanho-1;i>=0;i--){
+        printf("%c", nome[i]);  
+    }
+    printf("\n");
+    
+    printf("Informe uma letra para buscar: ");
+    scanf(" %c",&letra);
+    
+    printf("Busca pela letra %c\n",letra);
+    
+    for(i=0;i<tamanho;i++){
+        if(nome[i]==letra){
+            contador++;
+        }
+    }   
+    
+    if(contador>0){
+        if(contador==1){
+            printf("Foi encontrado %d resultado na string", contador);
+        }else{
+            printf("Foram encontrados %d resultados na string", contador);  
+        }
+    }else{
+        printf("Nao foi encontrada a respectiva letra no nome informado");
+    }
  
-	return 0;
+    contador = 0;
+    printf("Busca pela letra IGNORANDO case sensitive\n");
+    printf("Informe uma letra para buscar: ");
+    scanf(" %c",&letra);
+    
+    letra = tolower(letra);
+    
+    printf("Busca pela letra %c\n",letra);
+    
+    for(i=0;i<tamanho;i++){
+        if(tolower(nome[i])==letra){
+            contador++;
+        }
+    }   
+    
+    if(contador>0){
+        if(contador==1){
+            printf("Foi encontrado %d resultado na string", contador);
+        }else{
+            printf("Foram encontrados %d resultados na string", contador);  
+        }
+    }else{
+        printf("Nao foi encontrada a respectiva letra no nome informado");
+    }
+    
+    return 0;
 }
  
  
-// 1 - declaração
-int em_uma_decada(int minha_idade){
-	//printf("@ idade informada: %d\n",minha_idade);
-	return minha_idade + 10;	
+/*
+#include <stdio.h>
+#include <string.h>
+ 
+int main(){
+    char nome[50];
+    int tamanho = 0;
+        
+    printf("Informe o nome: ");
+    scanf("%[^\n]s",&nome);
+    
+    printf("Nome informado: %s\n", nome);
+    
+    printf("Primeira letra: %c\n",nome[0]);
+    
+    tamanho = strlen(nome);
+    
+    printf("Tamanho do nome %d",tamanho);
+    
+    return 0;
 }
-
-	
-	                        
-/* 	
-         _____
-		|    |
-		|    V
-     notas [10][20][30] X
-		   [0] [1] [2]
-
-   [Jit] -->								    Modularização							idade_mais_10 | idade: |  m.idade
-   -----             ____________				subprogamas									   Ø	  |   Ø	   |     Ø
-	a = 10;		    |    _____  |															 		  |  17	   |    17
-	b = 20;		    |	|	 |  |				funções: return.
-	c = [*b;]	   [0]  V	 | [3]			procedimentos: não tem retorno "void".
-				  [10][20] [x] [8]			
-	________	   a   b   c    d				3 momentos:
-	b = 30        2040 2041 2042 2043				declaração
-	               |			   |				protótipo
-|[int] notas[10];|  ---------------				chamada
---------------	        |
-	            		v
-	            		8c
-	            	10  30  30
 */
-	            	
